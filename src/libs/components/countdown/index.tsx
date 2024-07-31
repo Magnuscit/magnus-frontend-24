a703d36dddefffa7f5dd47fd6b6d5489c4bacdc6
Mukesh Kannan <mukeshkannan311@gmail.com>
2024-07-31 09:25:17 +0530

    feat: responsive

--- src/libs/components/countdown/index.tsx
+++ src/libs/components/countdown/index.tsx
@@ -14,30 +14,30 @@ const Countdown = () => {
       setTimeLeft(calculateTimeLeft());
     }, 1000);
     return () => clearInterval(timer);
-  }, []);
+  }, [timeLeft]);
   const formatTime = (unit: any) => String(unit).padStart(2, "0");
 
   return (
     <main
-      className={`h-screen w-full bg-purple-100 flex relative flex-col items-center justify-center select-none`}
+      className={`h-screen w-full bg-purple-100 flex relative flex-col items-center justify-center select-none md:p-0 p-10`}
     >
       <section className="flex flex-col items-center relative justify-center">
         <section className="h-2 w-full bg-black my-3" />
         <h1
-          className={`text-black text-[200px] leading-none font-black  ${anton.className}`}
+          className={`text-black text-8xl md:text-[200px] leading-none font-black  ${anton.className}`}
         >
           MAGNUS
         </h1>
         <section className="flex w-full justify-between px-1">
-          <h1 className="text-black text-3xl font-black self-end">
+          <h1 className="text-black text-xl md:text-3xl font-black self-end">
             T - {timeLeft.days} / {timeLeft.hours} / {timeLeft.minutes} /{" "}
             {formatTime(timeLeft.seconds)}
           </h1>
-          <h1 className="text-black text-3xl font-black self-end">2024</h1>
+          <h1 className="text-black text-xl md:text-3xl font-black self-end">2024</h1>
         </section>
         <section className="flex items-center space-x-4 justify-between w-full my-3">
           <section className="h-2 flex-grow bg-black" />
-          <h1 className="text-black font-bold">Presented by CIT</h1>
+          <h1 className="text-black font-bold">By CSE (AIML)</h1>
           <section className="h-2 flex-grow bg-black" />
         </section>
         <p className="w-96 text-black fontmono text-center text-xs">
