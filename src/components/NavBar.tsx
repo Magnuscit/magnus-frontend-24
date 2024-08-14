4b3b6be47f3cb0e67f276f2101949e471cab66b2
Mukesh Kannan <mukeshkannan311@gmail.com>
2024-08-14 10:14:52 +0530

    feat: herosection layout 🍉

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+
+export default function NavBar() {
+  const [isOpen, setOpen] = useState<boolean>(false);
+  const [activeNav, setActiveNav] = useState<boolean>(false);
+
+  const handleScroll = () => {
+    if (window.scrollY >= 50) setActiveNav(true);
+    else setActiveNav(false);
+  };
+
+  if (typeof window !== "undefined")
+    window.addEventListener("scroll", handleScroll);
+
+  return (
+    <nav
+      className={`
+        flex justify-between items-center w-full p-8 fixed top-0 z-[999] bg-nocl font-filgen ${
+          activeNav &&
+          "bg-white drop-shadow-md transition-all duration-200 ease-in"
+        }
+      `}
+    >
+      <Link href="/" className="text-3xl font-extrabold font-glb">
+      Magnus
+      </Link>
+
+      <section
+        className={`
+          ${!isOpen ? "-right-[1000px]" : "right-0"}
+          top-0 absolute md:static bg-white md:bg-nocl h-screen w-full md:w-auto md:h-auto flex items-end justify-center transition-all p-8 md:p-0
+          text-right text-6xl md:text-xl md:space-x-3 flex-col md:flex-row
+        `}
+      >
+        <Link href="/about">
+          <h1 className="hover:text-accent">About</h1>
+        </Link>
+        <Link href="/events">
+          <h1 className="hover:text-accent">Events</h1>
+        </Link>
+        <Link href="/upcoming-events">
+          <h1 className="hover:text-accent">Upcoming</h1>
+        </Link>
+        <Link href="/socials">
+          <h1 className="hover:text-accent">Socials</h1>
+        </Link>
+        <Link href="/team">
+          <h1 className="hover:text-accent">Team</h1>
+        </Link>
+        <div className="md:hidden block">hola</div>
+      </section>
+
+      <div className="flex items-center gap-x-2">
+        {/* <ProfileIcon /> */}
+        <div className="hidden md:block">{/* <GitHubLogin /> */}</div>
+        <h1
+          onClick={() => setOpen(!isOpen)}
+          className="cursor-pointer text-3xl md:hidden z-[1000]"
+        >
+          {isOpen ? "x" : "="}
+        </h1>
+      </div>
+    </nav>
+  );
+}
--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,9 +1,47 @@
+import NavBar from "../NavBar";
+
 const Main = () => {
-	return (
-		<div>
-			MAGNUS&apos;24 main page
-		</div>
-	)
-}
+  return (
+    <main className="relative min-h-screen w-full bg-black z-0">
+      <NavBar />
+
+      <section className="relative min-h-screen w-full flex flex-col items-center justify-center">
+        <div className="absolute right-0 left-0 mx-auto -top-20 bg-orange-400 w-96 h-96 blur-[170px] rounded-full -z-10"></div>
+        <div className="h-full w-2/3 absolute graph-paper -z-10" />
+        <h1 className="text-[150px] text-white font-black">MAGNUS</h1>
+        <button className="text-xl border text-white px-3 py-1 rounded-sm border-white">
+          Explore
+        </button>
+      </section>
+
+      <section className="min-h-screen w-full flex items-center justify-center px-32">
+        <div className="flex flex-col w-full h-full">
+          <h1 className="text-white font-semibold text-5xl">About Us</h1>
+          <p className="text-white text-base">
+            Magnus is the technical symposium of the department of
+            CSE(Artificial Intelligence and Machine Learning) at Chennai
+            Institute of Technology which was founded in 2023. With over a
+            footfall of 500 in its first year, it is an event that celebrates
+            the achievements in the field of AI along with Machine Learning. At
+            Magnus, participants get to immerse themselves in various activities
+            such as workshops on AI and Open-Source platforms, non-technical
+            events like Weeb Trivia and Film Fusion along with technical events
+            like paper and project presentations, motivating them to think
+            outside the box and incorporate AI in real life.
+          </p>
+        </div>
+        <div className="h-full w-full bg-white"></div>
+      </section>
+
+      <section className="min-h-screen w-full bg-purple-900 flex flex-col items-center justify-center p-24">
+        <h1 className="text-white font-bold text-7xl">Events</h1>
+        <div className="py-10 w-full bg-red-800 flex items-center justify-center gap-x-4">
+          <section className="h-64 w-56 bg-white"> </section>
+          <section className="h-64 w-56 bg-white"> </section>
+        </div>
+      </section>
+    </main>
+  );
+};
 
 export default Main;
