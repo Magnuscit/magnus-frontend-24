77ffec9c6ac1af8384e64bdb04a26e44b97a0e5a
Lovelin <100030865+lovelindhoni@users.noreply.github.com>
2025-01-29 20:52:07 +0530

    fix: title 2024 -> 2025

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,18 +5,19 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Magnus'24",
-	description: "A National level technical symposium organized by the Department of Artificial Intelligence and Machine Learning, Chennai Institute of Technology.",
+  title: "Magnus'25",
+  description:
+    "A National level technical symposium organized by the Department of Artificial Intelligence and Machine Learning, Chennai Institute of Technology.",
 };
 
-export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
-	return (
-		<html lang="en">
-			<body className={inter.className}>
-				<div className="h-screen w-screen">
-					{children}
-				</div>
-			</body>
-		</html>
-	);
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="h-screen w-screen">{children}</div>
+      </body>
+    </html>
+  );
 }
