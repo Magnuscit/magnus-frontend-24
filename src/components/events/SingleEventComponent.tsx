dc311a7326eae5cd45c17062b56f97c01d0c9e89
Lovelin <100030865+lovelindhoni@users.noreply.github.com>
2025-02-06 19:49:31 +0530

    SingleEventComponent fixes

--- src/components/events/SingleEventComponent.tsx
+++ src/components/events/SingleEventComponent.tsx
@@ -9,183 +9,198 @@ import { useAuth, useRazorpayStore } from "@/store";
 import RenderRazorpay from "../RenderRazorpay";
 
 interface User {
-	name: string;
-	clg_name: string;
-	phone_no: string;
-	event_id: string[];
+  name: string;
+  clg_name: string;
+  phone_no: string;
+  event_id: string[];
 }
 
 interface Order {
-	id: string;
-	entity: string;
-	amount: number;
-	amount_paid: number;
-	amount_due: number;
-	currency: string;
-	receipt: string;
-	offer_id: string | null;
-	status: string;
-	attempts: number;
-	notes: string[];
-	created_at: number;
+  id: string;
+  entity: string;
+  amount: number;
+  amount_paid: number;
+  amount_due: number;
+  currency: string;
+  receipt: string;
+  offer_id: string | null;
+  status: string;
+  attempts: number;
+  notes: string[];
+  created_at: number;
 }
 
 const SingleEventComponent = ({ eventData }: { eventData: EVENT }) => {
-	console.log(eventData)
-	const router = useRouter();
-	const { auth } = useAuth();
-	const { isRazorpayOpen, openRazorpay } = useRazorpayStore((e) => e);
+  console.log(eventData);
+  const router = useRouter();
+  const { auth } = useAuth();
+  const { isRazorpayOpen, openRazorpay } = useRazorpayStore((e) => e);
 
-	const [order, setOrder] = useState<Order | null>(null);
-	const [user, setUser] = useState<User | null>(null);
-	const [paymentDisabled, setPaymentDisabled] = useState(false);
+  const [order, setOrder] = useState<Order | null>(null);
+  const [user, setUser] = useState<User | null>(null);
+  const [paymentDisabled, setPaymentDisabled] = useState(false);
 
-	const handlePayment = async () => {
-		try {
-			const registrationResponse = await axios.get(
-				`${API_URL}/user/userFullyRegistered`,
-				{
-					withCredentials: true,
-					headers: {
-						Authorization: `Bearer ${auth?.jwt}`,
-						"Content-Type": "application/json",
-					},
-				}
-			);
+  const handlePayment = async () => {
+    try {
+      const registrationResponse = await axios.get(
+        `${API_URL}/user/userFullyRegistered`,
+        {
+          withCredentials: true,
+          headers: {
+            Authorization: `Bearer ${auth?.jwt}`,
+            "Content-Type": "application/json",
+          },
+        },
+      );
 
-			if (registrationResponse.data.status === "notok") {
-				router.push("/edit-name");
-				return;
-			}
+      if (registrationResponse.data.status === "notok") {
+        router.push("/edit-name");
+        return;
+      }
 
-			const orderResponse = await fetch(`${API_URL}/payment/createOrder`, {
-				method: "POST",
-				headers: {
-					Authorization: `Bearer ${auth?.jwt}`,
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({
-					amount: eventData.details.price * 100,
-					receipt: "receipt#2",
-					notes: { eventId: eventData.id },
-				}),
-			});
+      const orderResponse = await fetch(`${API_URL}/payment/createOrder`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${auth?.jwt}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          amount: eventData.details.price * 100,
+          receipt: "receipt#2",
+          notes: { eventId: eventData.id },
+        }),
+      });
 
-			const orderData = await orderResponse.json();
+      const orderData = await orderResponse.json();
 
-			if (orderData.status === "👍") {
-				openRazorpay();
-				setOrder(orderData.order);
-			} else {
-				alert("Failed to create order: " + orderData.message);
-			}
-		} catch (error) {
-			console.error("Error:", error);
-			alert("An error occurred. Please try again.");
-		}
-	};
+      if (orderData.status === "👍") {
+        openRazorpay();
+        setOrder(orderData.order);
+      } else {
+        alert("Failed to create order: " + orderData.message);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("An error occurred. Please try again.");
+    }
+  };
 
-	useEffect(() => {
-		const fetchUserData = async () => {
-			try {
-				const response = await axios.get(`${API_URL}/user/get`, {
-					withCredentials: true,
-					headers: {
-						Authorization: `Bearer ${auth?.jwt}`,
-						"Content-Type": "application/json",
-					},
-				});
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/user/get`, {
+          withCredentials: true,
+          headers: {
+            Authorization: `Bearer ${auth?.jwt}`,
+            "Content-Type": "application/json",
+          },
+        });
 
-				const userData: User = response.data.data;
-				setUser(userData);
+        const userData: User = response.data.data;
+        setUser(userData);
 
-				const hasRegisteredForEvent = userData.event_id.includes(eventData.title);
-				setPaymentDisabled(hasRegisteredForEvent);
-			} catch (error) {
-				console.error("Error fetching user data:", error);
-			}
-		};
+        const hasRegisteredForEvent = userData.event_id.includes(
+          eventData.title,
+        );
+        setPaymentDisabled(hasRegisteredForEvent);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
 
-		fetchUserData();
-	}, [auth?.jwt, eventData.title]);
+    fetchUserData();
+  }, [auth?.jwt, eventData.title]);
 
-	return (
-		<main className="bg-cream font-space uppercase w-full min-h-screen p-10 px-5 lg:px-24 pt-24">
-			<section className="event-header">
-				<h1 className="lg:text-8xl text-5xl font-bold text-accent flex flex-col lg:flex-row space-y-5 lg:space-y-0 lg:space-x-5 ">
-					<div className="w-full text-left">{eventData.title}</div>
-					<span className="bg-accent w-full text-white text-[50px] flex flex-col items-center justify-center font-bold text-center">{eventData.details.type}</span>
-				</h1>
-				<p className="lg:text-xl text-center font-medium text-gray-800 pb-5 mt-6">
-					{eventData.description}
-				</p>
-			</section>
+  return (
+    <main className="bg-cream font-space uppercase w-full min-h-screen p-10 px-5 lg:px-24 pt-24">
+      <section className="event-header">
+        <h1 className="lg:text-8xl text-5xl font-bold text-accent flex flex-col lg:flex-row space-y-5 lg:space-y-0 lg:space-x-5 ">
+          <div className="w-full text-left">{eventData.title}</div>
+          <span className="bg-accent w-full text-white text-[50px] flex flex-col items-center justify-center font-bold text-center">
+            {eventData.details.type}
+          </span>
+        </h1>
+        <p className="lg:text-xl text-center font-medium text-gray-800 pb-5 mt-6">
+          {eventData.description}
+        </p>
+      </section>
 
-			<hr />
+      <hr />
 
-			<section className="event-pricing">
-				{isRazorpayOpen && order && user && (
-					<RenderRazorpay
-						amount={order.amount}
-						currency={order.currency}
-						orderid={order.id}
-						keyid={process.env.REACT_APP_RAZORPAY_KEY_ID || ""}
-						user={user}
-					/>
-				)}
-			</section>
+      <section className="event-pricing">
+        {isRazorpayOpen && order && user && (
+          <RenderRazorpay
+            amount={order.amount}
+            currency={order.currency}
+            orderid={order.id}
+            keyid={process.env.REACT_APP_RAZORPAY_KEY_ID || ""}
+            user={user}
+          />
+        )}
+      </section>
 
-			<h2 className="text-5xl w-full font-black text-cream bg-accent text-center py-2">
-				{eventData.details.price === 0 ? "FREE" : `₹${eventData.details.price}`}
-			</h2>
+      <h2 className="text-5xl w-full font-black text-cream bg-accent text-center py-2">
+        {eventData.details.price === 0 ? "FREE" : `₹${eventData.details.price}`}
+      </h2>
 
+      <section className="lg:flex lg:space-x-10">
+        <div className="event-registration w1/2">
+          <h2 className="text-3xl mt-4 font-medium text-accent">
+            REGISTRATION
+          </h2>
+          <ul>
+            {eventData.registration.map((item, index) => (
+              <li key={index} className="text-justify font-medium pt-2">
+                {index + 1}. {item}
+              </li>
+            ))}
+          </ul>
+        </div>
 
-			<section className="lg:flex lg:space-x-10">
-				<div className="event-registration w1/2">
-					<h2 className="text-3xl mt-4 font-medium text-accent">REGISTRATION</h2>
-					<ul>
-						{eventData.registration.map((item, index) => (
-							<li key={index} className="text-justify font-medium pt-2">
-								{index + 1}. {item}
-							</li>
-						))}
-					</ul>
-				</div>
+        <div className="event-rules w1/2">
+          <h2 className="text-3xl mt-4 font-medium text-accent">RULES</h2>
+          <ul>
+            {eventData.rules.map((rule, index) => (
+              <li key={index} className="text-justify pt-2 font-medium">
+                {index + 1}. {rule}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
 
-				<div className="event-rules w1/2">
-					<h2 className="text-3xl mt-4 font-medium text-accent">RULES</h2>
-					<ul>
-						{eventData.rules.map((rule, index) => (
-							<li key={index} className="text-justify pt-2 font-medium">
-								{index + 1}. {rule}
-							</li>
-						))}
-					</ul>
-				</div>
-			</section>
+      <section className="event-contacts">
+        <h2 className="text-3xl mt-4 font-medium text-accent">CONTACT US!!</h2>
+        <ul className="pt-1 font-medium">
+          {eventData.contacts.map((contact, index) => (
+            <li key={index}>
+              {contact.incharge}: {contact.phno}
+            </li>
+          ))}
+        </ul>
+      </section>
 
-			<section className="event-contacts">
-				<h2 className="text-3xl mt-4 font-medium text-accent">CONTACT US!!</h2>
-				<ul className="pt-1 font-medium">
-					{eventData.contacts.map((contact, index) => (
-						<li key={index}>
-							{contact.incharge}: {contact.phno}
-						</li>
-					))}
-				</ul>
-			</section>
-
-			<div className="w-full flex flex-col items-center justify-center">
-				<a
-					// @ts-ignore
-					href={eventData["gform"]}
-					className="bg-accent cursor-pointer text-cream text-2xl font-bold px-3 py-2 mt-4 hover:bg-accent-hover"
-				>
-					* REGISTER *
-				</a>
-			</div>
-		</main>
-	);
+      <div className="w-full flex flex-col items-center justify-center">
+        {
+          //@ts-ignore
+          eventData["gform"] ? (
+            <a
+              // @ts-ignore
+              href={eventData["gform"]}
+              target="_blank"
+              className="bg-accent cursor-pointer text-cream text-2xl font-bold px-3 py-2 mt-4 hover:bg-accent-hover"
+            >
+              * REGISTER *
+            </a>
+          ) : (
+            <button className="bg-gray-500 text-white text-2xl font-bold px-3 py-2 mt-4 cursor-not-allowed">
+              Opening Shortly!
+            </button>
+          )
+        }
+      </div>
+    </main>
+  );
 };
 
 export default SingleEventComponent;
