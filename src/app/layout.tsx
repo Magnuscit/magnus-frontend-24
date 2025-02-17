fbaa07b607f79b997c813f7f997c3e76e110dffa
Lovelin <100030865+lovelindhoni@users.noreply.github.com>
2025-02-18 03:13:36 +0530

    feat: magnus 25 end modal and confetti

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,32 +4,34 @@ import "./globals.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { CLIENT_ID } from "@/libs/constants";
 import { BusRoute } from "@/components/bus-route";
+import PostMagnusModal from "@/components/PostMagnusModal";
 
 const inter = Inter({ subsets: ["latin"] });
 const space_grotesk = Space_Grotesk({
-	variable: "--space-grotesk",
-	subsets: ["latin"],
+  variable: "--space-grotesk",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-	title: "Magnus'25",
-	description:
-		"A National level technical symposium organized by the Department of Artificial Intelligence and Machine Learning, Chennai Institute of Technology.",
+  title: "Magnus'25",
+  description:
+    "A National level technical symposium organized by the Department of Artificial Intelligence and Machine Learning, Chennai Institute of Technology.",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{ children: React.ReactNode }>) {
-	return (
-		<GoogleOAuthProvider clientId={CLIENT_ID}>
-			<html lang="en">
-				<body className={`${inter.className} ${space_grotesk.variable}`}>
-					<div className="h-screen w-screen">
-						<BusRoute />
-						{children}
-					</div>
-				</body>
-			</html>
-		</GoogleOAuthProvider>
-	);
+  return (
+    <GoogleOAuthProvider clientId={CLIENT_ID}>
+      <html lang="en">
+        <body className={`${inter.className} ${space_grotesk.variable}`}>
+          <div className="h-screen w-screen">
+            {/* <BusRoute /> */}
+            <PostMagnusModal />
+            {children}
+          </div>
+        </body>
+      </html>
+    </GoogleOAuthProvider>
+  );
 }
